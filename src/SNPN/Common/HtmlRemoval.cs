37bc4a80b04aca98d5a7a205a8e9a0e6d3e1dfb6
Willie Zutz <boarder2@bit-shift.com>
2017-02-09 00:24:50 -0700

    Formatting HtmlRemoval

--- src/SNPN/Common/HtmlRemoval.cs
+++ src/SNPN/Common/HtmlRemoval.cs
@@ -7,16 +7,16 @@ namespace SNPN.Common
 		/// <summary>
 		/// Compiled regular expression for performance.
 		/// </summary>
-		static Regex tagsRegex = new Regex("<.*?>", RegexOptions.Compiled);
-		static Regex spoilerRegex = new Regex("<span class=\"jt_spoiler\" onclick=\"this.className = '';\">.*?</span>", RegexOptions.Compiled);
+		private static readonly Regex TagsRegex = new Regex("<.*?>", RegexOptions.Compiled);
+		private static readonly Regex SpoilerRegex = new Regex("<span class=\"jt_spoiler\" onclick=\"this.className = '';\">.*?</span>", RegexOptions.Compiled);
 
 		/// <summary>
 		/// Remove HTML from string with compiled Regex.
 		/// </summary>
 		public static string StripTagsRegexCompiled(string source)
 		{
-			var result = spoilerRegex.Replace(source, "______");
-			return tagsRegex.Replace(result, string.Empty);
+			var result = SpoilerRegex.Replace(source, "______");
+			return TagsRegex.Replace(result, string.Empty);
 		}
 	}
 }
