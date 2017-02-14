29763454946ad50d2835aa7e9c7294da0128dd65
Willie Zutz <boarder2@bit-shift.com>
2017-02-14 00:39:32 -0700

    Move some build helpers to a directory to unclutter root.

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+# Run from base project directory
 dotnet restore && dotnet build **/project.json && dotnet test test/SNPN.Test/
--- tools/setdebugtype.sh
+++ tools/setdebugtype.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+# Run from base project directory
 find . -type f -name "project.json" -exec sed -i 's/\"debugType\"[[:space:]]*:[[:space:]]*\"full\"/\"debugType\": \"portable\"/g' {} +
