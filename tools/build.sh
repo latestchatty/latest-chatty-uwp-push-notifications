0e128766792b59a01f3c708aa6407c52a2140e4a
Willie Zutz <boarder2@bit-shift.com>
2017-03-21 01:06:11 -0600

    Travis build.

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Run from base project directory
-dotnet restore && dotnet build && dotnet test test/SNPN.Test/SNPN.csproj
+dotnet restore
+dotnet build
+dotnet test test/SNPN.Test/SNPN.csproj
